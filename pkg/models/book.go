7d65648731c4c87e9f348b2cfeb3bc56c712db5b
Paul Fleischer-Djoleto <pfleischer2002@yahoo.co.uk>
2022-10-28 16:44:01 +0000

    Commiting models and utils

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,6 +1,19 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
 	"github.com/Djisu/go-bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
 )
+
+var db *gorm.DB
+
+type Book struct{
+	gorm.model
+	Name string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init(){
+	config.Connect()
+}
